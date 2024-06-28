47e4b88ac0b183ffa74be641d8c78f0998825871
Solomon <solomonuche@gmail.com>
2024-06-28 13:11:29 +0100

    fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
-
 import sys
+"""
+Supplies a script that reads stdin line by line and computes metrics
+"""
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+file_size = 0
+line_count = 0
 
-if __name__ == '__main__':
+try:
+    for line in sys.stdin:
+        split_line = line.split(" ")
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+        if len(split_line) > 4:
+            status_code = split_line[-2]
+            size = int(split_line[-1])
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
+            file_size += size
+            line_count += 1
+        if line_count == 10:
+            line_count = 0
+            print('File size: {}'.format(file_size))
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
-    try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
-    except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+            for key, value in sorted(status_codes.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+except Exception:
+    pass
+finally:
+    print('File size: {}'.format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
